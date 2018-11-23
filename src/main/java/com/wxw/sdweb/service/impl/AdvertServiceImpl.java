b3df0cc4ec5b39c3741c54ddcf07905861fd5a85
wxwbblx <wxwbblx@163.com>
2018-11-23 10:55:09 +0800

    广告服务层修改 2018-11-11

---
+++ src/main/java/com/wxw/sdweb/service/impl/AdvertServiceImpl.java
@@ -0,0 +1,51 @@
+package com.wxw.sdweb.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+import com.wxw.sdweb.dao.AdvertMapper;
+import com.wxw.sdweb.service.IAdvertService;
+import com.wxw.sdweb.vo.Advert;
+import com.wxw.sdweb.vo.Menu;
+@ComponentScan({ "com.wxw.sdweb.mapper" })
+@Service(value = "advertService")
+public class AdvertServiceImpl implements IAdvertService {
+
+	@Autowired
+	private AdvertMapper  advertDao;
+	
+	
+	@Override
+	public List<Advert> findAll() {
+		List<Advert> objs = advertDao.findAll();
+		return objs;
+	}
+
+	@Override
+	public Advert find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insert(Advert obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Advert obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
