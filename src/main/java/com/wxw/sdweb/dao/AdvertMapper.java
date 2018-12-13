3dc2470bf644718df1f2709d141ef8c14821f63a
wxwbblx <wxwbblx@163.com>
2018-12-13 21:34:46 +0800

    001

--- src/main/java/com/wxw/sdweb/dao/AdvertMapper.java
+++ src/main/java/com/wxw/sdweb/dao/AdvertMapper.java
@@ -14,22 +14,22 @@ import com.wxw.sdweb.vo.Advert;
 @Mapper
 public interface AdvertMapper {
 
-	@Select("select * from  Advert order by adtime desc")
+	@Select("select * from  advert order by adtime desc")
 	public List<Advert> findAll();
 	
-	@Select("select * from Advert where adpage  = #{adpage} and isenable='1'")
+	@Select("select * from advert where adpage  = #{adpage} and isenable='1'")
 	public List<Advert> findByPage(String adpage);
 
-	@Select("select * from Advert where id  = #{id}")
+	@Select("select * from advert where id  = #{id}")
 	public Advert find(int id);
 
-	@Select("select * from Advert where adname  = #{adname}")
+	@Select("select * from advert where adname  = #{adname}")
 	public Advert findbyname(String mname);
 
-	@Delete("delete from Advert where id  = #{id}")
+	@Delete("delete from advert where id  = #{id}")
 	public int delete(int id);
 
-	@Update("update Advert set adname=#{adname},adtitle=#{adtitle},adtype=#{adtype},adurl=#{adurl},adtime=#{adtime},adpage=#{adpage},isenable=#{isenable},remark=#{remark}  where id  = #{id}")
+	@Update("update advert set adname=#{adname},adtitle=#{adtitle},adtype=#{adtype},adurl=#{adurl},adtime=#{adtime},adpage=#{adpage},isenable=#{isenable},remark=#{remark}  where id  = #{id}")
 	public int update(Advert obj);
 
 	@Insert("insert into advert " + "(adname,adtitle,adtype,adurl,adtime,adpage,isenable,remark) "
